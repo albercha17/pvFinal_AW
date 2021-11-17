ee78fee747b3d2e38bd483485bd59395caf27841
albercha17 <64268285+albercha17@users.noreply.github.com>
2021-11-17 13:53:03 +0100

    muchas cosas y un router

--- app.js
+++ app.js
@@ -1,20 +1,5 @@
 "use strict";
 
-const mysql = require("mysql");
-const config = require("./JS/config");
-const DAOUsers = require("./JS/DAOUsers");
-// Crear el pool de conexiones
-const pool = mysql.createPool({
-    host: config.host,
-    user: config.user,
-    password: config.password,
-    database: config.database
-});
-let daoUser = new DAOUsers(pool);
-
-
-
-
 const path = require("path");
 const express = require("express");
 const {
@@ -24,22 +9,19 @@ const {
     response
 } = require("express");
 const app = express();
+const session= require("express-session")
+const cookieParser=require("cookie-parser")
 var router_user = require("./routers/users")
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 
-var usuario = new Object();
-usuario.nombre = null;
-usuario.email = null;
-usuario.img = null;
-usuario.identificado = false;
-usuario.contraseña = null;
-var errorCrearUsuario = null;
-var errorLogin = null;
-var log_o_SingUp = false;  // esto lo pongo para cuando no se esta identificado saber si ir al login o al singup
-
+const middelwareSession=session({
+    saveUninitialized:false,
+    secret:"foobar34",
+    resave:false
+});
 //MIDDELWARE  ------------------------------------------------------------------------------------------------------------------------------------
 //error404
 function middelware404Error(request, response, next) {
@@ -56,120 +38,18 @@ function middelware500Error(error, request, response, next) {
         pila: error.stack
     });
 };
-//comprobar que se esta logeado
-function identificador(request, response, next) {
-    if (usuario.identificado) {
-        next();
-    } else {
-        if (log_o_SingUp == false) {
-            response.redirect("/login");
-        } else {
-            response.redirect("/SingUp");
-            log_o_SingUp = false;
-        }
 
-    }
-}
-
-
+ app.use(cookieParser());
 
 // rutas-------------------------------------------------------------------------------------------------------------------------------------
 
-app.get("/login", function (request, response) {
-    if (usuario.identificado) {
-        response.redirect("/inicio");
-    } else {
-        response.render("login", {
-            errorLogin: errorLogin
-        });
-    }
-    errorLogin = null;
-});
-app.get("/SingUp", function (request, response) {
-    if (usuario.identificado) {
-        response.redirect("/inicio");
-    } else {
-        response.render("SingUp", {
-            errorCrearUsuario: errorCrearUsuario
-        });
-    }
-    errorCrearUsuario = null;
-});
-app.get("/", identificador, function (request, response) {
-    // response.status(200);
-    response.redirect("/inicio");
-});
-app.get("/inicio", identificador, function (request, response) {
-    // response.status(200);
-    response.render("inicio", {
-        nombre: usuario.nombre
-    });
-});
+app.get("/",router_user);
+app.get("/inicio",router_user);
+app.get("/login",router_user);
+app.get("/loguearse",router_user);
+app.get("/SingUp",router_user);
+app.get("/crearUsuario",router_user);
 
-
-// funciones de las vistas --------------------------------------------------------------------------------------------------------------
-
-// funcion logearse------------------------------------------------------------------------------------
-app.get("/logearse", function (request, response) {
-    daoUser.isUserCorrect(request.query.email, request.query.password, cb_isUserCorrect);
-    response.redirect("/inicio");
-});
-
-function cb_isUserCorrect(err, result) {
-    if (err) {
-        usuario.identificado = false;
-        console.log(err.message);
-    } else if (result) {
-        usuario.identificado = true;
-        daoUser.getUserName(result.email, buscarNombre);
-        console.log("Usuario y contraseña correctos");
-    } else {
-        usuario.identificado = false;
-        errorLogin = "Usuario y/o contraseña incorrectos";
-        console.log("Usuario y/o contraseña incorrectos");
-
-    }
-}
-
-
-//funcion crear usuario------------------------------------------------------------------------------------------------
-app.get("/crearUsuario", function (request, response) {
-    //validar datos
-    var img = imagenPerfil(request.query.img);
-    var datosValidados = validarDatos(request.query.email, request.query.password, request.query.password_r);
-
-    if (datosValidados) {
-        daoUser.insertUser(request.query.email, request.query.password, request.query.nombre, img, crearusuario);
-
-    }
-    log_o_SingUp = true;
-    response.redirect("/inicio");
-});
-
-function crearusuario(err, result) {
-    if (err) {
-        errorCrearUsuario = err;
-        usuario.identificado = false;
-        console.log(err.message);
-    } else if (result) {
-        usuario.identificado = true;
-        daoUser.getUserName(result, buscarNombre);
-        console.log("Usuario creado");
-    }
-}
-
-function buscarNombre(err, result) {
-    if (err) {
-        console.log(err.message);
-    } else if (result) {
-        usuario.nombre = result.nombre;
-        console.log("Usuario:" + result.email);
-    } else {
-        console.log("Usuario incorrecto");
-    }
-}
-
-//errores y listen--------------------------------------------------------------------------------------------------------------------------
 
 app.use(middelware404Error);
 app.use(middelware500Error);
@@ -191,38 +71,3 @@ app.listen(3000, function (err) {
 
 
 
-//funciones -----------------------------------------------------------------------------------------------------------------------------
-
-
-function validarDatos(email, contraseña, contraseña_r) {
-    var valido = false;
-    if (contraseña != contraseña_r) {
-        errorCrearUsuario = "Las contraseñas no coinciden";
-        console.log();
-    } else if (validarEmail(email) && contraseña == contraseña_r) valido = true;
-    return valido;
-}
-
-function validarEmail(email) {
-    var re = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
-    if (!re.exec(email)) {
-        errorCrearUsuario = "Correo no valido. Tiene que contener un @ con un . --> ejemplo: albercha@404.es";
-        console.log("Correo no valido");
-        return false;
-    } else return true;
-}
-
-function imagenPerfil(img) {
-    if (img) {
-        return img;
-    } else {
-        var listaImagenes = ['/Imagenes_de_perfil/sfg.png',
-            '/Imagenes_de_perfil/roberto.png', '/Imagenes_de_perfil/nico.png',
-            '/Imagenes_de_perfil/marta.png', '/Imagenes_de_perfil/kuroko.png',
-            '/Imagenes_de_perfil/defecto3.png', '/Imagenes_de_perfil/defecto2.png',
-            '/Imagenes_de_perfil/defecto1.png', '/Imagenes_de_perfil/amy.png'
-        ];
-        var numero = Math.floor(Math.random() * (listaImagenes.length - 0));
-        return listaImagenes[numero];
-    }
-}
--- routers/users.js
+++ routers/users.js
@@ -1,45 +1,167 @@
 var express = require('express');
-var router_login = express.Router();
+var router = express.Router(); // creo el router
+const path = require("path");
+const {request} = require("http");
+const {response} = require("express");
+//DAOS   -----------------------------------------------------
+const config = require("../JS/config");
+const mysql = require("mysql");
+const DAOUsers = require("../JS/DAOUsers");
+// Crear el pool de conexiones
+const pool = mysql.createPool({
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.database
+});
+let daoUser = new DAOUsers(pool);
+//--------------------------------------------------
 
-let usuario_identificado = false;
+
+//const session= require("express-session")
+//const cookieParser=require("cookie-parser")
 
 
-// middleware that is specific to this router_login
 
-function identificador(request,response,next){
-    if(usuario_identificado){
+var identificado=false;
+var errorLogin = null;
+var errorCrearUsuario=null;
+var nombre="";
+
+// middleware that is specific to this router
+function identificador(request, response, next) {
+    if (identificado) {
         next();
-    }
-    else{
-        response.redirect("/login");
+    } else {
+            response.redirect("/login");
     }
 }
-
 // define the home page route
-router_login.get("/inicio_Sesion", function(request, response) {
-    if(usuario_identificado){ response.render("secreto");}
-    else{ response.sendFile(path.join(__dirname, "./Views/Templates","login.html"));}
+router.get("/",identificador, function (request, response) {
+    // response.status(200);
+    response.redirect("/inicio");
+});
+router.get("/inicio",identificador, function (request, response) {
+    // response.status(200);
+    response.render("inicio", {
+        nombre: nombre
+    });
+});
+
+router.get("/login", function (request, response) {
+    if (identificado) {
+        response.redirect("/inicio");
+    } else {
+        response.render("login", {
+            errorLogin: errorLogin
+        });
+    }
+    errorLogin = null;
+});
+
+router.get("/SingUp", function (request, response) {
+    if (identificado) {
+        response.redirect("/login");
+    } else {
+        response.render("SingUp", {
+            errorCrearUsuario: errorCrearUsuario
+        });
+    }
+        
+    errorCrearUsuario = null;
 });
 
 
-router_login.get("/logearse", function (request, response) {
-    daoUser.isUserCorrect(request.query.nombre,request.query.password,cb_isUserCorrect);
-    response.redirect("/secreto");
+// funciones
+router.get("/loguearse", function (request, response) {
+    daoUser.isUserCorrect(request.query.email, request.query.password, cb_isUserCorrect);
+    response.redirect("/inicio");
 });
-
-
-//callbacks
 function cb_isUserCorrect(err, result) {
     if (err) {
-        usuario_identificado = false;
+        identificado = false;
         console.log(err.message);
     } else if (result) {
-        usuario_identificado = true;
+        identificado = true;
+        daoUser.getUserName(result.email, buscarNombre);
         console.log("Usuario y contraseña correctos");
     } else {
-        usuario_identificado = false;
+        identificado = false;
+        errorLogin = "Usuario y/o contraseña incorrectos";
         console.log("Usuario y/o contraseña incorrectos");
+
+    }
+}
+function buscarNombre(err, result) {
+    if (err) {
+        console.log(err.message);
+    } else if (result) {
+        nombre = result.nombre;
+        console.log("Usuario:" + result.email);
+    } else {
+        console.log("Usuario incorrecto");
+    }
+}
+router.get("/crearUsuario", function (request, response) {
+    //validar datos
+    var img = imagenPerfil(request.query.img);
+    var datosValidados = validarDatos(request.query.email, request.query.password, request.query.password_r);
+
+    if (datosValidados) {
+        daoUser.insertUser(request.query.email, request.query.password, request.query.nombre, img, crearusuario);
+    }
+        response.redirect("/SingUp");
+    
+});
+
+function crearusuario(err, result) {
+    if (err) {
+        errorCrearUsuario = err;
+        console.log(err.message);
+    } else if (result) {
+        identificado=true;
+        daoUser.getUserName(result, buscarNombre);
+        console.log("Usuario creado");
     }
 }
 
-module.exports = router_login;
+
+//funciones -----------------------------------------------------------------------------------------------------------------------------
+
+
+function validarDatos(email, contraseña, contraseña_r) {
+    var valido = false;
+    if (contraseña != contraseña_r) {
+        errorCrearUsuario = "Las contraseñas no coinciden";
+        console.log();
+    } else if (validarEmail(email) && contraseña == contraseña_r) valido = true;
+    return valido;
+}
+
+function validarEmail(email) {
+    var re = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+    if (!re.exec(email)) {
+        errorCrearUsuario = "Correo no valido. Tiene que contener un @ con un . --> ejemplo: albercha@404.es";
+        console.log("Correo no valido");
+        return false;
+    } else return true;
+}
+
+function imagenPerfil(img) {
+    if (img) {
+        return img;
+    } else {
+        var listaImagenes = ['/Imagenes_de_perfil/sfg.png',
+            '/Imagenes_de_perfil/roberto.png', '/Imagenes_de_perfil/nico.png',
+            '/Imagenes_de_perfil/marta.png', '/Imagenes_de_perfil/kuroko.png',
+            '/Imagenes_de_perfil/defecto3.png', '/Imagenes_de_perfil/defecto2.png',
+            '/Imagenes_de_perfil/defecto1.png', '/Imagenes_de_perfil/amy.png'
+        ];
+        var numero = Math.floor(Math.random() * (listaImagenes.length - 0));
+        return listaImagenes[numero];
+    }
+}
+
+
+
+module.exports = router;
